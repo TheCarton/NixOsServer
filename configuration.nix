8434fe626de4ea2261e9605a7e32c4517894d5d5
luke <theukearchy@gmail.com>
2024-07-12 12:32:52 -0400

    dum

--- configuration.nix
+++ configuration.nix
@@ -86,6 +86,7 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+    wormhole-rs
     helix
     lazygit
     nil
@@ -99,6 +100,7 @@
     jellyfin
     jellyfin-web
     jellyfin-ffmpeg
+    nginx
   ];
 
   environment.variables.EDITOR = "helix";
