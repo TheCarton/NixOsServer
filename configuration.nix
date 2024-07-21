677fcc7b478de1c7e19daac77e8142fde8a5055c
TheCarton <theukearchy@gmail.com>
2024-07-21 15:06:02 -0400

    idk

--- configuration.nix
+++ configuration.nix
@@ -135,7 +135,6 @@
   services.mullvad-vpn.enable = true;
 
   environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     mullvad
     sonarr
     transmission-qt
