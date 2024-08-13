f2126dab2bb76b72c63b02ba2f382aacba63b52d
TheCarton <theukearchy@gmail.com>
2024-08-13 18:59:56 -0400

    y i k e s

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -8,7 +8,7 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
@@ -22,11 +22,6 @@
     { device = "/dev/disk/by-uuid/0A85-6620";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
-    };
-
-  fileSystems."/data" =
-    { device = "/dev/disk/by-uuid/f10e65af-b5ea-498f-9920-b09d7aa0ac41";
-      fsType = "ext4";
     };
 
   swapDevices = [ ];
