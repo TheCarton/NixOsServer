47b9ab125a25462c8189bc5a50b3916ccbff994c
TheCarton <theukearchy@gmail.com>
2025-02-27 12:10:15 -0500

    works somehow but no https

--- luke-vandermale-site/configuration.nix
+++ luke-vandermale-site/configuration.nix
@@ -6,6 +6,12 @@
   ];
 
   # stolen from https://matoking.com/blog/2023/07/08/deploying-hugo-site-using-nixos-and-nginx/#end
+  # also stolen from https://prodlog.xyz/posts/hugo-nix/
+  #
+  # TODO section
+  # Bafflingly, the static site is available at lukevandermale.com.
+  # (1) HTTPS isn't working, I need to fix the certificate.
+  # (2) I don't know what it's using the default site and not the weird theme stolen from prodlog.
 
   nixpkgs.overlays = [
     (self: super: rec {
